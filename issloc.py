ce98d040a7e360498a8f7ae5e4179fa3415dfd58
mtos <tkddruid@gmail.com>
2021-03-09 00:22:49 -0500

    IssLoc recommit

--- issloc.py
+++ issloc.py
@@ -1,5 +1,10 @@
 import plotly.express as px
 import pandas as pd
-url = 'http://open-notify.org/Open-Notify-API/ISS-Location-Now/'
+url = 'http://api.open-notify.org/iss-now.json'
 df = pd.read_json(url)
-print(df)
+df['latitude'] = df.loc['latitude','iss_position']
+df['longitude'] = df.loc['longitude', 'iss_position']
+df.reset_index(inplace=True)
+df = df.drop(['index','message'], axis=1)
+fig = px.scatter_geo(df, lat='latitude', lon='longitude')
+fig.show()
